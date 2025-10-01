150736c44f7e63d0e8978442120b4e2278d79cd3
Dmitry Shashkov (Brookit) <brookit18@gmail.com>
2025-10-01 23:31:15 +0300

    Add enderman drop

---
+++ src/main/java/io/github/brookite/items/RareEnderPearlItem.java
@@ -0,0 +1,9 @@
+package io.github.brookite.items;
+
+import net.minecraft.item.EnderPearlItem;
+
+public class RareEnderPearlItem extends EnderPearlItem {
+    public RareEnderPearlItem(Settings settings) {
+        super(settings);
+    }
+}
