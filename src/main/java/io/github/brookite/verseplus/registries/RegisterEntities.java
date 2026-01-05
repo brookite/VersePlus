a5162d53a12c12158318527f001a003149b67a47
Dmitry Shashkov (Brookit) <brookit18@gmail.com>
2026-01-05 21:31:46 +0300

    Refactor entity registry

--- src/main/java/io/github/brookite/verseplus/registries/RegisterEntities.java
+++ src/main/java/io/github/brookite/verseplus/registries/RegisterEntities.java
@@ -2,6 +2,7 @@ package io.github.brookite.verseplus.registries;
 
 import io.github.brookite.verseplus.VersePlus;
 import io.github.brookite.verseplus.entities.FireEnderPearlEntity;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.registry.Registries;
@@ -11,16 +12,21 @@ import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
 public class RegisterEntities {
-    private static final RegistryKey<EntityType<?>> FIRE_ENDER_PEARL_KEY =
-            RegistryKey.of(RegistryKeys.ENTITY_TYPE, Identifier.of(VersePlus.MOD_ID, "fire_ender_pearl_entity"));
-
     // Объявляем тип сущности
-    public static final EntityType<FireEnderPearlEntity> FIRE_ENDER_PEARL_ENTITY = Registry.register(
-            Registries.ENTITY_TYPE,
-            Identifier.of(VersePlus.MOD_ID, "fire_ender_pearl_entity"),
+    public static final EntityType<FireEnderPearlEntity> FIRE_ENDER_PEARL_ENTITY = register("fire_ender_pearl_entity",
             EntityType.Builder.<FireEnderPearlEntity>create(FireEnderPearlEntity::new, SpawnGroup.CREATURE)
-                    .dimensions(0.25f, 0.25f).build(FIRE_ENDER_PEARL_KEY)
+            .dimensions(0.25f, 0.25f)
     );
+
+    private static <T extends Entity> EntityType<T> register(String name, EntityType.Builder<T> type) {
+        RegistryKey<EntityType<?>> regKey =
+                RegistryKey.of(RegistryKeys.ENTITY_TYPE, Identifier.of(VersePlus.MOD_ID, name));
+        return Registry.register(
+                Registries.ENTITY_TYPE,
+                Identifier.of(VersePlus.MOD_ID, name),
+                type.build(regKey)
+        );
+    }
 
     // Метод инициализации
     public static void initialize() {
